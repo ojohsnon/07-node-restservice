893854239ab0b98d76a8ecd1b818e03aa62b475e
ojohsnon <oskijlchino@gmail.com>
2020-07-06 15:46:29 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,23 @@
 //==========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+//==========================
+// Entorno
+//==========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//==========================
+// Base de datos
+//==========================
+
+let urlDB;
+/*
+if (process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else {*/
+    urlDB = 'mongodb+srv://kakur:J8NDdmf2mbyPUCbu@cluster0.9pahp.mongodb.net/cafe';
+//}
+
+process.env.URLDB = urlDB;
